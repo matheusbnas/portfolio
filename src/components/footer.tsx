3a92f7ad2344f2779bc04d4cc05aca03c2cd7eff
Matheus Bernardes <matheusbnas@gmail.com>
2025-02-04 16:50:57 -0300

    Uodate: Footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
 
 export default function Footer({ language }) {
   return (
     <footer className="mt-8 pt-4 border-t border-gray-700 text-center text-gray-400">
-      <p>&copy; 2025 Matheus Bernardes. {language === 'pt' ? 'Todos os direitos reservados.' : 'All rights reserved.'}</p>
+      <p>
+        &copy; 2025 Matheus Bernardes.{" "}
+        {language === "pt"
+          ? "Todos os direitos reservados."
+          : "All rights reserved."}
+      </p>
     </footer>
   );
 }
