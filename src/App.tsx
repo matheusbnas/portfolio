75ab90fa44cd9f628255e4d841d9a4cccbc5de25
Matheus Bernardes <matheusbnas@gmail.com>
2025-02-04 17:14:55 -0300

    Fix> Arquivo Footer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Blog from "./components/Blog";
-import Footer from "./components/Footer.jsx";
+import Footer from "./components/footer.tsx";
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
